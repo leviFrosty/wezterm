9d104524104502d97f8676572690047395a37366
Levi Wilkerson <levi.wilkerson@paulaschoice.com>
2024-04-25 19:00:30 -0400

    feat: test

--- .wezterm.lua
+++ .wezterm.lua
@@ -15,6 +15,7 @@ end
 -- For example, changing the color scheme:
 -- config.color_scheme = 'AdventureTime'
 config.font = wezterm.font("FiraCode Nerd Font")
+
 config.font_size = 22
 
 -- and finally, return the configuration to wezterm
