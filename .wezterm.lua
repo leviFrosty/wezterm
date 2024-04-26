f2977a3fad2cb2b541415c01211e0af86e79361b
Levi Wilkerson <levi.wilkerson@paulaschoice.com>
2024-04-25 22:23:00 -0400

    feat: adjust font and colors

--- .wezterm.lua
+++ .wezterm.lua
@@ -15,8 +15,28 @@ end
 -- For example, changing the color scheme:
 -- config.color_scheme = 'AdventureTime'
 config.font = wezterm.font("FiraCode Nerd Font")
+config.font_size = 18
+config.line_height = 1.15
 
-config.font_size = 22
+config.colors = {
+	background = "#242424",
+	foreground = "#ffffff",
+}
+
+config.window_padding = {
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
+}
+
+config.window_frame = {
+	font_size = 10,
+}
+
+config.window_close_confirmation = "NeverPrompt"
+
+config.default_cwd = "~/documents/"
 
 -- and finally, return the configuration to wezterm
 return config
