c69af9ca3c729eec7f9713e66b7cb4c57e33a60e
Levi Wilkerson <levi.wilkerson@paulaschoice.com>
2024-04-28 22:31:07 -0400

    feat: add alpha dashboard

--- .config/nvim/lua/vim-options.lua
+++ .config/nvim/lua/vim-options.lua
@@ -1,6 +1,6 @@
 -- Noevide
 if vim.g.neovide then
-  vim.o.guifont = 'FiraCode Nerd Font:h20' -- This is only for neovide
+  vim.o.guifont = 'FiraCode Nerd Font:h18' -- This is only for neovide
   vim.g.neovide_fullscreen = true
   vim.g.neovide_cursor_animation_length = 0.04
   vim.g.neovide_cursor_trail_size = 0.6
